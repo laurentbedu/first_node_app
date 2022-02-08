6a21d0ed156e87127b5ebd18c3ed4056968dd113
laurentbedu <bedulaurent@gmail.com>
2022-02-08 09:44:52 +0100

    V1 finished

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("my first node app");
+const express = require("express");
+const router = express.Router();
+
+const app = express();
+
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:3000"]
+};
+app.use(cors(corsOptions));
+
+app.use(router);
+
+router.route("/")
+.get((req, res) => {
+  res.send((req.method + req.path));
+})
+.post((req, res) => {
+  res.send((req.method + req.path));
+});
+
+router.route("/test").get((req, res) => {
+    res.send((req.method + req.path));
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
